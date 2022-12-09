e84fbc41517e960b6305f72e16791cce2c1f56a9
Andrew0613 <puyuandong01061313@gmail.com>
2022-12-09 14:26:59 +0800

    update yaml and paired dataset

--- basicsr/models/image_restoration_model.py
+++ basicsr/models/image_restoration_model.py
@@ -323,10 +323,11 @@ class ImageRestorationModel(BaseModel):
                 H,W = ((h+factor)//factor)*factor, ((w+factor)//factor)*factor
                 padh = H-h if h%factor!=0 else 0
                 padw = W-w if w%factor!=0 else 0
-                X = max(h+padh, w+padw)
-                padw = X-w
-                padh = X-h
-                self.lq = F.pad(self.lq, (0,padw,0,padh), 'constant', 0)
+                # X = max(h+padh, w+padw)
+                # padw = X-w
+                # padh = X-h
+                # self.lq = F.pad(self.lq, (0,padw,0,padh), 'constant', 0)
+                self.lq = F.pad(self.lq, (0,padw,0,padh), 'reflect')
                 # print("Image size: ",h,w)
                 # print("Padding to",H,W)
                 # print("self.lq.size()", self.lq.size())
